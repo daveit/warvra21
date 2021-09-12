185a7bf0b8735be8003151622f22e6676d58917f
daveit <daveporter@me.com>
2021-09-12 11:54:47 +0800

    tidy up of css

--- app/models/village.rb
+++ app/models/village.rb
@@ -7,7 +7,7 @@ class Village < ActiveRecord::Base
 
   has_many :contacts
   has_many :vreceipts
-  has_many :residents 
+  has_many :residents
   
   validates :name, presence: true
   validates_uniqueness_of :name
